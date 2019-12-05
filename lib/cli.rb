ada662b0ddb93c3ae3fa1b72fdd7b2d8f2d74348
jasmosez <jas.schaffer@gmail.com>
2019-12-05 15:20:45 -0500

    tweaked welcome and clear screen

--- lib/cli.rb
+++ lib/cli.rb
@@ -16,12 +16,12 @@ class Cli
   end
   
   def self.welcome           
-                                                                                                                                                                                                              
+    clear_screen
+    ascii                                                                                                                                                                                                          
     puts "Hey, Slugger! Welcome to AllSTarzBaseball"
   end
 
   def self.login_prompt
-    
     email = PROMPT.ask("Email:")
   end
 
@@ -103,7 +103,6 @@ class Cli
   end
 
   def self.status_message(status)
-    clear_screen
     case status
     when "empty"
       
@@ -429,6 +428,27 @@ class Cli
 
   def self.clear_screen
     system 'clear'
+  end
+
+  def self.ascii
+    puts ""
+    puts ""
+    puts ""
+    puts ""
+
+    puts '       d8888 888 888  .d8888b. 88888888888                        888888b.                              888               888 888'.colorize(:red)
+    puts '      d88888 888 888 d88P  Y88b    888                            888  "88b                             888               888 888'.colorize(:red)
+    puts '     d88P888 888 888 Y88b.         888                            888  .88P                             888               888 888'.colorize(:red)
+    puts '    d88P 888 888 888  "Y888b.      888   8888b.  888d888 88888888 8888888K.   8888b.  .d8888b   .d88b.  88888b.   8888b.  888 888'.colorize(:white)
+    puts '   d88P  888 888 888     "Y88b.    888      "88b 888P"      d88P  888  "Y88b     "88b 88K      d8P  Y8b 888 "88b     "88b 888 888'.colorize(:white)
+    puts '  d88P   888 888 888       "888    888  .d888888 888       d88P   888    888 .d888888 "Y8888b. 88888888 888  888 .d888888 888 888'.colorize(:white)
+    puts ' d8888888888 888 888 Y88b  d88P    888  888  888 888      d88P    888   d88P 888  888      X88 Y8b.     888 d88P 888  888 888 888'.colorize(:blue)
+    puts 'd88P     888 888 888  "Y8888P"     888  "Y888888 888     88888888 8888888P"  "Y888888  88888P"  "Y8888  88888P"  "Y888888 888 888'.colorize(:blue)
+                                                                                                                                
+    puts ""
+    puts ""
+    puts ""
+    puts ""
   end
 
   # STRETCH METHODS
